77588959b55392078aa514aacb7b15ce82890ce2
Brandon Maxwell <theill1b22@gmail.com>
2021-12-20 16:49:03 -0600

    Figured out how to get the boxes all even. Now need to figure out how to get dots added.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Create a grid
+// Create dots
+
+// Lines turn from white to black when clicked
+
+// Turn number font in boxes white/hidden
+
+// When adjacent column & row lines are selected, creating a box, alert
+
+// When above occurs, have the text inside be replaced with colored font. Preferrably the initial of the player.  If too complicated, then let it be P1/P2
+
+// create logic that will count the number of boxes for both players
+
+// create logic that will choose a winner
+
+// Create CPU competition (if time)
+
+

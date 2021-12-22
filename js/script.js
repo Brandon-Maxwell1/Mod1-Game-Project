4df97d8e2e35058e57cc3f8fe917b0b04ed1ebc7
Brandon Maxwell <theill1b22@gmail.com>
2021-12-22 17:47:36 -0600

    changed the game again. This time to coin-flip.  I think I have a better grasp at how to do this one

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-// Create dots
-
-// Lines turn from white to black when clicked
-
-// Turn number font in boxes white/hidden
-
-// When adjacent column & row lines are selected, creating a box, alert
-
-// When above occurs, have the text inside be replaced with colored font. Preferrably the initial of the player.  If too complicated, then let it be P1/P2
-
-// create logic that will count the number of boxes for both players
-
-// create logic that will choose a winner
-
-// Create CPU competition (if time)
+// ===DOM Entries===
 
 
+// Player 1 choose heads or tail
+
+function addPlayerName1(){
+    let input = document.getElementById('nameEntry')
+    let div = document.getElementById('player1Name')    
+    div.innerHTML = div.innerHTML + input.value;
+    }
+
+function addPlayerName2(){
+    let input = document.getElementById('nameEntry')
+    let div = document.getElementById('player2Name')
+    div.innerHTML = div.innerHTML + input.value;
+}
+
+
+function choose(){
+let heads = 1
+let tails = 2
+let randNum = Math.round((Math.random() * (2 - 1)) + 1)
+
+if(p1 === heads && randNum === 1)
+console.log('HEADS: p1 wins')
+else{
+    console.log('TAILS: p2 wins')
+}
+}
